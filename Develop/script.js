9e336034f0b6b5bbd5bec069987b7dbc8e88191d
JamesJ995 <mrjam7@gmail.com>
2021-05-16 17:11:36 -0500

    Added generatepassword function and some working prompts for password character types

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charSet = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -8,6 +9,29 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  var length = prompt("How long would you like your password to be? Please choose between 8 and 128 characters.");
+  if(length < 8 || length > 128){
+    alert("Password must be between 8 and 128 characters, please try again.");
+    return;
+  }
+  var underCase = confirm("Would you like to use undercase characters?");
+  if(underCase){
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+  }
+  var upperCase = confirm("Would you like to use uppercase characters?");
+  if(upperCase){
+    var addUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    charSet = charSet.concat(addUpperCase);
+  }
+  var numeric = confirm("Would you like to use numeric characters?")
+  if(numeric){
+    var addNumeric = "0123456789";
+    charSet = charSet.concat(addNumeric);
+  }
+  return;
 }
 
 // Add event listener to generate button
